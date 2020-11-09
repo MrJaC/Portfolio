013016226889812a88fe8ab1753e157e40090467
Jodi <jodicoetzee@live.com>
2020-11-09 16:09:26 +0200

    updates

--- src/app/service/project-data.service.ts
+++ src/app/service/project-data.service.ts
@@ -14,6 +14,7 @@ export class ProjectDataService {
         id: 1,
         title: "Intouch Advertising",
         type: "Website",
+        date: "",
         mainImage: "assets/images/default.jpg",
         images: [
           {
@@ -106,6 +107,7 @@ export class ProjectDataService {
         id: 2,
         title: "Intouch Advertising",
         type: "Mobile Application",
+        date: "",
         mainImage: "assets/images/default.jpg",
         images: [
           {
@@ -192,6 +194,7 @@ export class ProjectDataService {
         id: 3,
         title: "Oil & Gas Connect",
         type: "Mobile Application",
+        date: "",
         mainImage:
           "assets/projects/oilgasconnect/feature_one_white_connect.png",
         image: [
@@ -279,6 +282,7 @@ export class ProjectDataService {
         id: 4,
         title: "Oil & Gas Connect",
         type: "Backend - Laravel",
+        date: "",
         mainImage: "assets/images/default.jpg",
         image: [
           {
@@ -365,6 +369,7 @@ export class ProjectDataService {
         id: 5,
         title: "Oil & Gas Connect",
         type: "Backend - Laravel",
+        date: "",
         mainImage: "assets/images/default.jpg",
         image: [
           {
@@ -451,6 +456,7 @@ export class ProjectDataService {
         id: 6,
         title: "Oil & Gas Connect",
         type: "Backend - Laravel",
+        date: "",
         mainImage: "assets/images/default.jpg",
         image: [
           {
@@ -537,6 +543,7 @@ export class ProjectDataService {
         id: 7,
         title: "Oil & Gas Connect",
         type: "Backend - Laravel",
+        date: "",
         mainImage: "assets/images/default.jpg",
         image: [
           {
@@ -618,6 +625,99 @@ export class ProjectDataService {
         description: "This is a descrition",
         blurb: "This is a blurb",
         url: "https://google.co.za",
+      },
+      {
+        id: 8,
+        title: "SADL Decryptor",
+        type: "Java",
+        date: "",
+        mainImage: "assets/images/default.jpg",
+        images: [
+          {
+            img: "assets/images/default.jpg",
+          },
+        ],
+        tech: {
+          wordpress: [
+            {
+              show: true,
+              name: "WordPress",
+              logo: "logo-wordpress",
+            },
+          ],
+          php: [
+            {
+              show: true,
+              name: "PHP",
+              logo: null,
+            },
+          ],
+          java: [
+            {
+              show: false,
+              name: "Java",
+              logo: null,
+            },
+          ],
+          javascript: [
+            {
+              show: true,
+              name: "JavaScript",
+              logo: "logo-javascript",
+            },
+          ],
+          angular: [
+            {
+              show: false,
+              name: "Angular",
+              logo: "logo-Angular",
+            },
+          ],
+          css: [
+            {
+              show: true,
+              name: "CSS",
+              logo: "logo-css3",
+            },
+          ],
+          html: [
+            {
+              show: true,
+              name: "HTML5",
+              logo: "logo-html5",
+            },
+          ],
+          ionic: [
+            {
+              show: true,
+              name: "Ionic",
+              logo: "logo-ionic",
+            },
+          ],
+          laravel: [
+            {
+              show: true,
+              name: "Laravel",
+              logo: "logo-laravel",
+            },
+          ],
+          bootstrap: [
+            {
+              show: true,
+              name: "Bootstrap",
+              logo: null,
+            },
+          ],
+        },
+        description: [
+          {
+            text_one:
+              "InTouch Advertising required a quick CMS, as they wanted to updated themselves. Standard Theme via Themeforest installation and setup - with slight customization.",
+            text_two: "",
+          },
+        ],
+        blurb: "InTouch Advertising required a quick CMS...",
+        url: "https://intouchadvertising.co.za",
       },
     ];
 
