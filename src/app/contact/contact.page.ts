b822c6ea6757b44801d4642bb70178a1d321b988
Jodi <jodicoetzee@live.com>
2021-01-17 16:37:34 +0200

    updates

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -6,15 +6,33 @@ import { FormBuilder, FormGroup, Validators , FormControl} from "@angular/forms"
   styleUrls: ["./contact.page.scss"],
 })
 export class ContactPage implements OnInit {
+  
   public submitAttempt: boolean = false;
   public messageForm: FormGroup;
+  
   constructor(public formBuilder: FormBuilder) {
+
     this.messageForm = formBuilder.group({
       name: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
-      email: [''],
-      message: [''],
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
+      message: ['', Validators.required, Validators.pattern('[a-zA-Z ]*')],
     });
   }
 
   ngOnInit() {}
+  get errorControl() {
+    return this.messageForm.controls;
+  }
+  sendMessage(){
+    this.submitAttempt = true;
+    if (!this.messageForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.messageForm.value)
+    }
+  }
+
 }
