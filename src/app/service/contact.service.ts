021e8e580851236b4fa7a3afecbf923fed657d87
Jodi <jodicoetzee@live.com>
2021-01-24 14:38:24 +0200

    updates

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { environment } from "src/environments/environment";
+import { Observable, throwError } from "rxjs";
+import { retry, catchError, map } from "rxjs/operators";
+const SERV_URL: any = {
+  sendMail: environment.emailUrl + "email"
+}
+const httpOptions = {
+  headers: new HttpHeaders({ "Content-Type": "application/json" }),
+};
 @Injectable({
   providedIn: 'root'
 })
@@ -11,9 +23,30 @@ export class ContactService {
   }
 
 
-  
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error("An error occurred:", error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError("Something bad happened; please try again later.");
+  }
+  private extractData(res: Response) {
+    let body = res;
+    return body || {};
+  }
 
-
+  sendMail(data): Observable<any>{
+    return this.http.post(SERV_URL.sendMail, {
+      email: data["data"]
+    });
+  }
 
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true
+  production: false,
+  emailUrl: "http://test.jodicoetzee.co.za/php/email"
 };
 
 /*
