665a8d9b484b12325e2407d860ca2018fbb96532
Jodi <jodicoetzee@live.com>
2021-01-24 14:23:40 +0200

    updates

--- src/app/service/contact.service.ts
+++ src/app/service/contact.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { 
+
+  }
+
+
+  
+
+
+
+
 }
